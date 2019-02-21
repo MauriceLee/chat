0767497b8894d23f81251d92e056cf1af754893a
MauriceLee <ee.maurice.lee@gmail.com>
2019-02-22 03:11:47 +0800

    add converter

--- chat.py
+++ chat.py
@@ -3,3 +3,19 @@ with open('input.txt', 'r', encoding = 'utf-8') as f:
     for line in f:
         chat.append(line.strip())
 print(chat)
+
+output = []
+who = 0
+for line in chat:
+    if 'Allen' in line:
+        who = 1
+        continue
+    elif 'Tom' in line:
+        who = 2
+        continue
+    elif who == 1:
+        output.append('Allen: ' + line)
+    else:
+        output.append('Tom: ' + line)
+
+print(output)
