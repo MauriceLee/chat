7df4e51c5046e87a2848b91f9148720b6b8bd31f
MauriceLee <ee.maurice.lee@gmail.com>
2019-02-22 03:24:06 +0800

    add refactor

--- chat.py
+++ chat.py
@@ -1,25 +1,32 @@
-chat = []
-with open('input.txt', 'r', encoding = 'utf-8') as f:
-    for line in f:
-        chat.append(line.strip())
-print(chat)
+def read_file(filename):
+    chat = []
+    with open(filename, 'r', encoding = 'utf-8') as f:
+        for line in f:
+            chat.append(line.strip())
+    print(chat)
+    return chat
 
-output = []
-who = 0
-for line in chat:
-    if 'Allen' in line:
-        who = 1
-        continue
-    elif 'Tom' in line:
-        who = 2
-        continue
-    elif who == 1:
-        output.append('Allen: ' + line)
-    else:
-        output.append('Tom: ' + line)
+def write_file(filename, output):
+    with open(filename, 'w', encoding = 'utf-8') as f:
+        for out in output:
+            f.write(out + '\n')
 
-print(output)
+def main():
+    chat = read_file('input.txt')
+    output = []
+    who = 0
+    for line in chat:
+        if 'Allen' in line:
+            who = 1
+            continue
+        elif 'Tom' in line:
+            who = 2
+            continue
+        elif who == 1:
+            output.append('Allen: ' + line)
+        else:
+            output.append('Tom: ' + line)
+    print(output)
+    write_file('output.txt', output)
 
-with open('output.txt', 'w', encoding = 'utf-8') as f:
-    for out in output:
-        f.write(out + '\n')
+main()
