9abb198c3bcd298def6d50fc98a30cfe3bf012f4
MauriceLee <ee.maurice.lee@gmail.com>
2019-02-22 04:17:44 +0800

    add optimization

--- chat.py
+++ chat.py
@@ -1,32 +1,35 @@
 def read_file(filename):
     chat = []
-    with open(filename, 'r', encoding = 'utf-8') as f:
+    with open(filename, 'r', encoding = 'utf-8-sig') as f:
         for line in f:
             chat.append(line.strip())
     print(chat)
     return chat
 
+def convert(chat):
+    output = []
+    person = None
+    for line in chat:
+        if line == 'Allen':
+            person = 'Allen'
+            continue
+        elif line == 'Tom':
+            person = 'Tom'
+            continue
+        if person:
+            output.append(person + ': ' + line)
+    print(output)
+    return output
+
+
 def write_file(filename, output):
-    with open(filename, 'w', encoding = 'utf-8') as f:
+    with open(filename, 'w', encoding = 'utf-8-sig') as f:
         for out in output:
             f.write(out + '\n')
 
 def main():
     chat = read_file('input.txt')
-    output = []
-    who = 0
-    for line in chat:
-        if 'Allen' in line:
-            who = 1
-            continue
-        elif 'Tom' in line:
-            who = 2
-            continue
-        elif who == 1:
-            output.append('Allen: ' + line)
-        else:
-            output.append('Tom: ' + line)
-    print(output)
+    output = convert(chat)
     write_file('output.txt', output)
 
 main()
